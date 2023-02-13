4e5b448aa5f6da35d2141a9570cd17f7628d097f
W.H.P.Fernando <95269066+Hima-K@users.noreply.github.com>
2023-02-13 19:49:38 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,22 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2706
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Monaco;}
+{\colortbl;\red255\green255\blue255;\red38\green38\blue38;\red255\green255\blue255;}
+{\*\expandedcolortbl;;\cssrgb\c20000\c20000\c20000;\cssrgb\c100000\c100000\c100000;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs24 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 # Python Program to find Diameter, Circumference, and Area Of a Circle\
+\
+PI = 3.14\
+radius = float(input(' Please Enter the radius of a circle: '))\
+\
+diameter = 2 * radius\
+circumference = 2 * PI * radius\
+area = PI * radius * radius\
+\
+print(" \\nDiameter Of a Circle = %.2f" %diameter)\
+print(" Circumference Of a Circle = %.2f" %circumference)\
+print(" Area Of a Circle = %.2f" %area)\
+}
