5e7c8e60110934cfbbdcfbe1da5aa0fb40b29123
W.H.P.Fernando <95269066+Hima-K@users.noreply.github.com>
2023-02-13 19:45:54 +0530

    Add files via upload

---
+++  Finding area and circumference.py
@@ -0,0 +1,40 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2706
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 CourierNewPSMT;\f1\fmodern\fcharset0 CourierNewPS-BoldMT;\f2\fnil\fcharset0 Monaco;
+}
+{\colortbl;\red255\green255\blue255;\red93\green108\blue125;\red242\green242\blue242;\red28\green28\blue63;
+\red12\green99\blue153;\red249\green249\blue249;\red135\green91\blue44;\red255\green255\blue255;\red85\green138\blue3;
+\red135\green135\blue135;\red133\green0\blue67;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c43922\c50196\c56471;\cssrgb\c96078\c96078\c96078;\cssrgb\c14510\c15294\c31373;
+\cssrgb\c0\c46667\c66667;\cssrgb\c98039\c98039\c98039\c98039;\cssrgb\c60392\c43137\c22745;\cssrgb\c100000\c100000\c100000\c50196;\cssrgb\c40000\c60000\c0;
+\cssrgb\c60000\c60000\c60000;\cssrgb\c60000\c0\c33333;\cssrgb\c0\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs38 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 # Finding area and circumference\cf4 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 \
+\
+\cf2 \strokec2 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 # importing math module for PI\cf4 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 \
+\pard\pardeftab720\partightenfactor0
+
+\f1\b\fs32 \cf5 \cb6 \strokec5 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 import
+\f0\b0\fs38 \cf4 \cb3 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0  math\
+\
+\pard\pardeftab720\partightenfactor0
+\cf2 \strokec2 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 # Reading temperature in Celsius\cf4 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 \
+radius \cf7 \cb8 \strokec7 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 =\cf4 \cb3 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0  \cf9 \strokec9 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 float\cf10 \strokec10 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 (\cf9 \strokec9 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 input\cf10 \strokec10 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 (\cf9 \strokec9 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 'Enter radius of circle: '\cf10 \strokec10 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 ))\cf4 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 \
+\
+\cf2 \strokec2 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 # Calculating area and circumference\cf4 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 \
+area \cf7 \cb8 \strokec7 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 =\cf4 \cb3 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0  math\cf10 \strokec10 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 .\cf4 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 pi \cf7 \cb8 \strokec7 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 *\cf4 \cb3 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0  radius \cf7 \cb8 \strokec7 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 **\cf4 \cb3 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0  \cf11 \strokec11 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 2\cf4 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 \
+circumference \cf7 \cb8 \strokec7 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 =\cf4 \cb3 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0  \cf11 \strokec11 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 2\cf4 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0  \cf7 \cb8 \strokec7 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 *\cf4 \cb3 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0  math\cf10 \strokec10 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 .\cf4 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 pi \cf7 \cb8 \strokec7 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 *\cf4 \cb3 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0  radius\
+\
+\cf2 \strokec2 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 # Displaying output\cf4 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 \
+\pard\pardeftab720\partightenfactor0
+
+\f1\b\fs32 \cf5 \cb6 \strokec5 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 print
+\f0\b0\fs38 \cf10 \cb3 \strokec10 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 (\cf9 \strokec9 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 'Area = %0.4f.'\cf4 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0  \cf7 \cb8 \strokec7 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 %\cf4 \cb3 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0  \cf10 \strokec10 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 (\cf4 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 area\cf10 \strokec10 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 ))\cf4 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 \
+
+\f1\b\fs32 \cf5 \cb6 \strokec5 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 print
+\f0\b0\fs38 \cf10 \cb3 \strokec10 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 (\cf9 \strokec9 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 'Circumference = %0.4f.'\cf4 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0  \cf7 \cb8 \strokec7 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 %\cf4 \cb3 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0  \cf10 \strokec10 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 (\cf4 \strokec4 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 circumference\cf10 \strokec10 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 ))
+\f2\fs32 \cf0 \strokec12 \shad\shadx0\shady-20\shadr0\shado255 \shadc0 \
+}
